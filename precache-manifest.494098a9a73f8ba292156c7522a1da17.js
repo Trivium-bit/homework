8cfcd94638711f8815f6acfc7e517da871b12c9e
Trivium-bit <trivium.exe@gmail.com>
2021-10-23 08:20:12 +0300

    Updates

---
+++ precache-manifest.494098a9a73f8ba292156c7522a1da17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ceff97e37cf920f5fbbdb6ccad698e6",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "225d0674af6c0ccb1912",
+    "url": "/homework/static/css/main.c69b2ab8.chunk.css"
+  },
+  {
+    "revision": "2a6872e995d7da155a4a",
+    "url": "/homework/static/js/2.a6035f86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework/static/js/2.a6035f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225d0674af6c0ccb1912",
+    "url": "/homework/static/js/main.652624df.chunk.js"
+  },
+  {
+    "revision": "8217a5d2c8eff62f32e7",
+    "url": "/homework/static/js/runtime-main.c49b04a8.js"
+  }
+]);
