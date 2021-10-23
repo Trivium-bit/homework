e10919670fb73e0831f0553df80cd1e62587c80b
Trivium-bit <trivium.exe@gmail.com>
2021-10-23 13:27:45 +0300

    hw5

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Routes from "./Routes";
-/* import HashRouter from "react-router-dom" */
+import {HashRouter} from "react-router-dom"
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-
-           {/* <HashRouter> */}
+           <HashRouter>
            <Header/>
-            <Routes/>
-            {/* </HashRouter>  */}
+           <Routes/>  
+            </HashRouter> 
 
             
         </div>
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import {Switch,Route, Redirect} from "react-router-dom"
 import PreJunior from './pages/PreJunior'
 import Error404 from './pages/Error404'
+import Junior from './pages/Junior'
+import JuniorPlus from './pages/JuniorPlus'
 
 export const PATH = {
     PRE_JUNIOR: "/pre-junior",
+    JUNIOR: "/junior",
+    JUNIOR_PLUS: "/junior-plus"
     // add paths
 }
+
 
 function Routes() {
     return (
@@ -17,9 +22,11 @@ function Routes() {
             {/*в начале мы попадаем на страницу "/" и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после "/" ничего не будет)*/}
             <Route path={"/"} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
-
+            
             <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
-                // add routes
+            <Route path={PATH.JUNIOR} render={() => <Junior/>}/>
+            <Route path={PATH.JUNIOR_PLUS} render={() => <JuniorPlus/>}/>
+             
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
             <Route render={() => <Error404/>}/>
