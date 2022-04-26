136f77e73673f0634838df785e763ac8d755772a
Trivium-bit <trivium.exe@gmail.com>
2022-04-26 08:06:44 +0300

    hw11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from './common/c7-SuperRange/SuperRange.module.css'
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+    const onChangeRange = (value1: number) => setValue1(value1)
 
     return (
         <div>
@@ -12,20 +14,20 @@ function HW11() {
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
+            <div className={s.inputRange}>
+                <span className={s.span}>{value1}</span>
+                <SuperRange onChangeRange={onChangeRange} 
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
-            <div>
+{/*             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
-            </div>
+            </div> */}
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
