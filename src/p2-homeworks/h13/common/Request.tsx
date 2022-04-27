50728a7144952ace4551a0dc9684823e7f97b4e7
Trivium-bit <trivium.exe@gmail.com>
2022-04-27 11:40:12 +0300

    hw13

--- src/p2-homeworks/h13/common/Request.tsx
+++ src/p2-homeworks/h13/common/Request.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import Button from "./Button";
-import Checkbox from "./Checkbox";
-import s from './Request.module.css'
+import SuperButton from "../../h4/common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../../h4/common/c3-SuperCheckbox/SuperCheckbox";
+import { requestAPI } from "../api/RequestsAPI";
+
+
+
+const changeSuccess = (success: boolean) => {
+    requestAPI.changeSuccess(success)
+        .then(response => {
+            console.log(response.data)
+            return response.data
+        })
+        .catch(error => {console.log({ ...error });
+        console.log(error.response ? error.response.data.errorText : error.message);
+    })
+}
 
 function Request() {
     return (
-        <div className={s.request}>
-            <Button />
-            <Checkbox />
-{/*          <button>push</button>
-         <input type="checkbox"></input> */}
+        <div>
+            <SuperButton onClick={() => { changeSuccess(true) }}> Push </SuperButton>
+            <SuperCheckbox changeSuccess={changeSuccess} />
         </div>
     );
 }
