b56234aca908a56e9c18c6f42cf84f58c8d41c3e
Trivium-bit <trivium.exe@gmail.com>
2022-04-27 12:00:25 +0300

    p

--- src/p2-homeworks/h13/common/Request.tsx
+++ src/p2-homeworks/h13/common/Request.tsx
@@ -3,13 +3,11 @@ import SuperButton from "../../h4/common/c2-SuperButton/SuperButton";
 import SuperCheckbox from "../../h4/common/c3-SuperCheckbox/SuperCheckbox";
 import { requestAPI } from "../api/RequestsAPI";
 
-
-
 const changeSuccess = (success: boolean) => {
     requestAPI.changeSuccess(success)
         .then(response => {
-            console.log(response.data)
-            return response.data
+            const data = response.data
+            console.log(data)
         })
         .catch(error => {console.log({ ...error });
         console.log(error.response ? error.response.data.errorText : error.message);
@@ -21,6 +19,7 @@ function Request() {
         <div>
             <SuperButton onClick={() => { changeSuccess(true) }}> Push </SuperButton>
             <SuperCheckbox changeSuccess={changeSuccess} />
+            {}
         </div>
     );
 }
